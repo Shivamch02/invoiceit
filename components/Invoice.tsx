6c5e583d56a851763759de4605cafd8b59a51446
Shivam Chaurasiya <chshivam53@gmail.com>
2025-01-11 10:29:53 +0530

    updated

--- components/Invoice.tsx
+++ components/Invoice.tsx
@@ -39,8 +39,8 @@ export const Invoice = () => {
     const options = {
       margin: 10,
       filename: "invoice.pdf",
-      image: { type: "jpeg", quality: 0.5 }, // Lower image quality
-      html2canvas: { scale: 2, useCORS: true }, // Reduce scale and enable CORS
+      image: { type: "jpeg", quality: 0.5 },
+      html2canvas: { scale: 2, useCORS: true },
       jsPDF: {
         unit: "mm",
         format: "a4",
