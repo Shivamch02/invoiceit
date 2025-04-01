4aa6a5466b3c055b1de6a3a048d2498a2e637fbc
Shivam Chaurasiya <chshivam53@gmail.com>
2025-04-01 11:33:49 +0530

    updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="flex sm:flex-row flex-col items-center justify-between px-6 py-10">
-      <div className="font-bold text-3xl py-2">Invoiceit</div>
+      <div className="font-bold text-3xl py-2">Invoicenow</div>
       <div className="dark:text-gray-400 text-md text-gray-800">
         All rights reserved @2024
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <div className="flex justify-between items-center px-2 py-3 border border-gray-600 rounded-xl">
       <div className="font-bold sm:text-3xl text-2xl px-2">
-        <Link href={"/"}>Invoiceit.</Link>
+        <Link href={"/"}>Invoicenow.</Link>
       </div>
       <div className="gap-2 flex items-center">
         <button className="px-4 py-2.5 rounded-lg border border-gray-600 sm:block hidden">
